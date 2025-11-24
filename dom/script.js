7ebade36382b646557a9ac37157c1a8a7cbdaf0f
Dishant <2wodevs@gmail.com>
2025-11-25 01:02:42 +0530

    dom stickey notes

--- dom/script.js
+++ dom/script.js
@@ -1,4 +1,4 @@
-let box = document.querySelector("#box");
+let main = document.querySelector("main");
 
 let btn = document.querySelector("#btn");
 
@@ -7,5 +7,18 @@ btn.addEventListener("click", function () {
   let c2 = Math.floor(Math.random() * 256);
   let c3 = Math.floor(Math.random() * 256);
 
-  box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  let x = Math.random() * 80;
+  let y = Math.random() * 80;
+  let rotate = Math.random() * 360;
+
+  let div = document.createElement("div");
+  div.style.height = "180px";
+  div.style.width = "180px";
+  div.style.position = "absolute";
+  div.style.left = x + "%";
+  div.style.top = y + "%";
+  div.style.rotate = rotate + "deg";
+  div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+
+  main.appendChild(div);
 });
