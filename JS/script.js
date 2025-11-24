ec83eb45cefa4bba13a7dbdd1eb777ecd2c6aa98
Dishant <2wodevs@gmail.com>
2025-11-25 00:33:38 +0530

    Dom_Box color change

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// let start = +prompt("Enter start number");
+// let end = +prompt("Enter end number");
+
+// for (let i = start + 1; i < end; i++) {
+//   console.log(i);
+// }
+
+// let count = 0;
+// for (let i = 1; i <= 20; i++) {
+//   if (count === 3) break;
+//   if (i % 2 !== 0 && count < 3) {
+//     console.log(i);
+//     count++;
+//   }
+// }
